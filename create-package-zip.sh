9d9be875f1ea775a567ebe37cfe6672d0d708f44
Luis Weich <luis.weich.de@gmail.com>
2024-01-12 19:58:20 +0100

    add create-package-zip.sh

---
+++ create-package-zip.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+package_dir="package"
+package_zip="$package_dir/package.zip"
+
+./mvnw --batch-mode clean package >/dev/null
+
+if [ -d $package_dir ]; then
+  rm $package_dir/*.jar
+else
+  mkdir $package_dir
+fi
+
+cp target/*.jar $package_dir
+if [ -f $package_zip ]; then
+  rm $package_zip
+fi
+
+zip $package_zip -j -r $package_dir >/dev/null
